0db5a985e17ac3215b9b637cd4714767658f5f64
Avex12 <111732556+Avex12@users.noreply.github.com>
2023-08-27 18:47:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const editableDiv = document.querySelector("#editable_div");
+const heading = document.querySelector("#heading");
+displayHeading();
+
+// Event Listener
+editableDiv.addEventListener("click", editDiv);
+
+// Edit Div Function
+function editDiv() {
+  const existingTextArea = editableDiv.querySelectorAll("textarea");
+  if (existingTextArea.length !== 0) return;
+  const textArea = document.createElement("textarea");
+  heading.style.display = "none";
+  editableDiv.appendChild(textArea);
+  textArea.textContent = heading.textContent;
+  textArea.addEventListener("blur", (e) => {
+    saveToLocalStorage(textArea.value);
+    displayHeading();
+  });
+}
+
+// Save to Local Storage
+function saveToLocalStorage(textContent) {
+  return localStorage.setItem("heading", textContent);
+}
+
+// Read from Local Storage
+function readFromLocalStorage() {
+  return localStorage.getItem("heading");
+}
+
+// Display the data
+function displayHeading() {
+  heading.style.display = "block";
+  heading.textContent = readFromLocalStorage();
+  const existingTextArea = editableDiv.querySelectorAll("textarea");
+  if (existingTextArea.length !== 0) {
+    editableDiv.removeChild(existingTextArea[0]);
+  }
+}
